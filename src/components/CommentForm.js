6b88131f79eed7d0110f11af04940da11e5696fc
Andrew Mackay <andrewmackayd@gmail.com>
2021-10-12 11:16:15 +0100

    4th commit main

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,10 +1,27 @@
 
-import React from "react";
+import React, {useState} from "react";
 
 const CommentForm = () => {
+
+    const [author, setAuthor] = useState("")
+    const [text, setText] = useState("")
+
+    const handleAuthorChange = (event) => {
+        setAuthor(event.target.value)
+    }
+
+    const handleTextChange = (event) => {
+        setText(event.target.value)
+    }
+
     return(
         <div className="comment-form">
-            <p>I am a form..</p>
+            <h3>Add a comment:</h3>
+            <form>
+                <input type="text" placeholder="Your name.." value={author} onChange={handleAuthorChange}/>
+                <input type="text" placeholder="Say something.." value={text} onChange={handleTextChange}/>
+                <input type="submit" value="Add"/>
+            </form>
         </div>
     )
 }
