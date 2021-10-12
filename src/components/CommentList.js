07e8ec92656e9631c6b558a0efad43eaf44fbc4d
Andrew Mackay <andrewmackayd@gmail.com>
2021-10-12 10:19:57 +0100

    1st commit main

---
+++ src/components/CommentList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Comment from './Comment'
+
+const CommentList = ({comments}) => {
+
+    const commentNodes = comments.map((comment) => {
+        return <Comment author={comment.author} key={comment.id}>{comment.text}</Comment>
+    })
+
+  return (
+      <div className="comment-list">
+        <h2>Comments</h2>
+        {commentNodes}
+      </div>
+  )
+
+}
+
+export default CommentList;
---
+++ src/containers/CommentBox.js
@@ -0,0 +1,29 @@
+import React, {useState} from "react";
+import CommentList from "../components/CommentList";
+
+const CommentBox = () => {
+
+    const [comments, setComments] = useState(
+        [
+          {
+            id: 1,
+            author: "Rick Henry",
+            text: "React is such a great framework!"
+          },
+          {
+            id: 2,
+            author: "Valerie Gibson",
+            text: "I'm dreaming in React..."
+          }
+        ]
+      )
+
+  return (
+    <div className="comment-box">
+        <CommentList comments={comments} />
+    </div>
+  );
+
+}
+
+export default CommentBox;
